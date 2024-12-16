0f7d0fa1ec51a0436b47c6c7c3a543da23c01fdf
Mauke <mauke0781@gmail.com>
2024-12-16 20:44:29 +0000

    add new

--- ems/api/index.js
+++ ems/api/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(
    cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: "https://campus-backend-oxyd.onrender.com",
    })
 );
 
